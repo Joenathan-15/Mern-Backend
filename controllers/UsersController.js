fe0dcbbcff4ac7bc71c1dba8b14dc804bfe39ebf
Joenathan <hazendas10@gmail.com>
2023-05-18 11:56:06 +0700

    add Jwt (jsonwebtoken) idk what tf is that

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 
 const registerUser = asyncHandler(async (req,res) => {
@@ -30,11 +31,33 @@ const registerUser = asyncHandler(async (req,res) => {
 });
 
 const loginUser = asyncHandler(async (req,res) => {
-    res.json({message:"login user"})
+    const {email,password} = req.body
+    if(!email || !password){
+        res.status(400)
+        throw new Error("All field are require")
+    }
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user:{
+                username: user.username,
+                email: user.email,
+                id : user.id,
+            },
+        },
+        process.env.ACCESS_TOKEN_SECERT,
+        {expiresIn:"1m"}
+        );
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401)
+        throw new Error("Email or Password is not Valid")
+    }
 });
 
+
 const currentUser = asyncHandler(async (req,res) => {
-    res.json({message:"current user info"})
+    res.json(req.user);
 });
 
 module.exports = {registerUser,loginUser, currentUser}
