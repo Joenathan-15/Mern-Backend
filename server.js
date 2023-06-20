6c935a17e49a131a56440839b119a9624a086092
Joenathan <hazendas10@gmail.com>
2023-06-20 18:52:23 +0700

    fix mongodb connection

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express');
-const connectDb = require('./config/DBconnection');
 const errorHandler = require('./middleware/errorhandler');
 const dotenv = require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 
-connectDb()
+const connectDb = async () => {
+    try{
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log("DATABASE CONNECT: ", connect.connection.host, connect.connection.name)
+    }catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+app.all('*', (req,res) => {
+    res.json({"message":"Page Not Found"})
+})
 app.use(express.json());
 app.use('/api/users', require("./routes/UsersRoutes"));
 app.use('/api/blog', require("./routes/BlogRoutes"));
 app.use(errorHandler);
 
-app.listen(port,() =>{
-    console.log(`server running in port ${port}`)
+
+
+connectDb().then(() => {
+    app.listen(port,() =>{
+        console.log(`server running in port ${port}`)
+    })
 })
