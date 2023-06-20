d089a11db69510bfe49570bec6f16b3f92e49f4a
Joenathan <hazendas10@gmail.com>
2023-06-20 17:56:33 +0700

    fix .env and delete console.log

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -20,7 +20,6 @@ const registerUser = asyncHandler(async (req,res) => {
         email,
         password : hashPassword
     })
-    console.log(user)
     if(user){
         res.status(201).json({_id:user.id,email:user.email});
     }else{
